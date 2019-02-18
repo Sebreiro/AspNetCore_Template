834bbb1a8893f561dd11e450febe0f5c2e57ad4b
Sebreiro <admin@local.local>
2019-02-19 01:05:45 +0300

    Add test WebApi Controller

---
+++ AspNetCoreTemplate.Api/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreTemplate.Api.Controllers
+{
+    [Route("api/v1/[controller]/[action]/{id?}")]
+    public class TestController: ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "GET_HelloWorld";
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string str)
+        {
+            return Ok();
+        }
+
+    }
+}
